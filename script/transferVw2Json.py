142491634c812783cdd71565136c23562742a22d
dulun <dulun2834@126.com>
2018-05-01 10:10:07 +0800

    finish convert

--- script/transferVw2Json.py
+++ script/transferVw2Json.py
@@ -10,13 +10,29 @@ def main():
     args = parser.parse_args()
     input_file = os.path.join(ORIGIN_DATA_PATH, args.input_file)
     output_file = input_file + ".json"
+    data = []
     with open(input_file, "r") as f:
         for line in f:
             line = line.strip()
             if len(line) == 0:
                 continue
             items = line.split('|')
-            print items
-
+            dic = {}
+            for item in items:
+                item = item.strip()
+                if len(item) == 0:
+                    continue
+                its = item.split()
+                if len(its) <= 1:
+                    continue
+                if len(its) == 2:
+                    dic[its[0]] = int(its[1])
+                else:
+                    dic[its[0]] = []
+                    for it in its[1:]:
+                        dic[its[0]].append(int(it))
+            data.append(dic)
+    with open(output_file, "w") as f:
+        f.write(json.dumps(data))
 if __name__ == "__main__":
     main()
